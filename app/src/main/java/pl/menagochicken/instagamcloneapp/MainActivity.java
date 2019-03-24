de887b5e9edf4ee9fe25ec3456f449056e5a0a88
MenagoChicken <lewandowski.michau@gmail.com>
2019-03-24 19:01:27 +0100

    connecting to parse server

--- app/src/main/java/pl/menagochicken/instagamcloneapp/MainActivity.java
+++ app/src/main/java/pl/menagochicken/instagamcloneapp/MainActivity.java
@@ -1,7 +1,8 @@
 package pl.menagochicken.instagamcloneapp;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 }
