1e5892175570db195cbd07a0cd7ec1fe52c0d604
MenagoChicken <lewandowski.michau@gmail.com>
2019-03-24 21:21:21 +0100

    setup login/signin activity

--- app/src/main/java/pl/menagochicken/instagamcloneapp/MainActivity.java
+++ app/src/main/java/pl/menagochicken/instagamcloneapp/MainActivity.java
@@ -6,10 +6,20 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
+
+    public void switchLogInSignUp(){
+
+    }
+
+    public void  logInSignUp(){
+
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
 
     }
 }
