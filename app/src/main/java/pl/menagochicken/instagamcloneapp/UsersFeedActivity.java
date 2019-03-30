b8ba555117ffd33c2030ee5d5bc538b46464439e
MenagoChicken <lewandowski.michau@gmail.com>
2019-03-30 09:25:55 +0100

    getting users feed from server

--- app/src/main/java/pl/menagochicken/instagamcloneapp/UsersFeedActivity.java
+++ app/src/main/java/pl/menagochicken/instagamcloneapp/UsersFeedActivity.java
@@ -2,11 +2,23 @@ package pl.menagochicken.instagamcloneapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+
+import com.parse.FindCallback;
+import com.parse.GetDataCallback;
+import com.parse.ParseException;
+import com.parse.ParseFile;
+import com.parse.ParseObject;
+import com.parse.ParseQuery;
+
+import java.util.List;
 
 public class UsersFeedActivity extends AppCompatActivity {
 
@@ -15,15 +27,56 @@ public class UsersFeedActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_users_feed);
 
-        LinearLayout linearLayout =findViewById(R.id.linearLayout);
+        final LinearLayout linearLayout = findViewById(R.id.linearLayout);
 
-        ImageView imageView = new ImageView(getApplicationContext());
-        imageView.setLayoutParams(new ViewGroup.LayoutParams(
-                ViewGroup.LayoutParams.MATCH_PARENT, //width
-                ViewGroup.LayoutParams.WRAP_CONTENT //height
-        ));
+        //pobranie zawartości intentu
+        Intent intent = getIntent();
+        String activeUsername = intent.getStringExtra("username");
 
-        imageView.setImageDrawable(getResources().getDrawable(R.drawable.logo));
-        linearLayout.addView(imageView);
+        //ustwienie tytułu strony aplikacji
+        setTitle(activeUsername);
+
+        //pobranie wszsytkich zdjęć użytkownika
+        ParseQuery<ParseObject> query = new ParseQuery<ParseObject>("Image");
+        //select
+        query.whereEqualTo("username", activeUsername);
+        query.orderByAscending("createdAt");
+
+        query.findInBackground(new FindCallback<ParseObject>() {
+            @Override
+            public void done(List<ParseObject> objects, ParseException e) {
+                if (e == null) {
+
+                    if (objects.size() > 0) {
+                        for (ParseObject object : objects) {
+                            ParseFile file = (ParseFile) object.get("image");
+
+                            file.getDataInBackground(new GetDataCallback() {
+                                @Override
+                                public void done(byte[] data, ParseException e) {
+                                    if (e == null && data != null) {
+                                        Bitmap bitmap = BitmapFactory.decodeByteArray(data, 0, data.length);
+
+                                        ImageView imageView = new ImageView(getApplicationContext());
+
+                                        imageView.setLayoutParams(new ViewGroup.LayoutParams(
+                                                ViewGroup.LayoutParams.MATCH_PARENT, //width
+                                                ViewGroup.LayoutParams.WRAP_CONTENT //height
+                                        ));
+
+                                        imageView.setImageBitmap(bitmap);
+                                        linearLayout.addView(imageView);
+                                    }
+
+                                }
+                            });
+                        }
+                    }
+
+                }
+            }
+        });
+
+
     }
 }
